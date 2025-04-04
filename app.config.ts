ffb1c6671fca49f74728033f0e546429bd5f4491
Thomas <thomas.giudici@epitech.eu>
2025-04-04 17:07:14 +0200

    done

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: { preset: "node-server", compatibilityDate: "2025-03-30" },
+  server: { preset: "vercel" },
   vite: { plugins: [tailwindcss()] },
 });
